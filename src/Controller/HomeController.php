d0389d6e9f89646f2d739e62e20b2c791da3b69d
arefeen <arefeen.git@gmail.com>
2023-07-16 06:13:01 +0600

    Scraper done and optimized

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,28 +12,30 @@ class HomeController extends AbstractController {
 
     #[Route('/home', name: 'app_home')]
     public function index(): Response {
-        // Set the URL
+        
+        // Set URL
         $url = Constants::SCRAP_FROM . 'en/company-search/1/';
-        $cookie_consent = 'CookieScriptConsent=%7B%22googleconsentmap%22%3A%7B%22ad_storage%22%3A%22targeting%22%2C%22analytics_storage%22%3A%22performance%22%2C%22functionality_storage%22%3A%22functionality%22%2C%22personalization_storage%22%3A%22functionality%22%2C%22security_storage%22%3A%22functionality%22%7D%2C%22action%22%3A%22accept%22%2C%22categories%22%3A%22%5B%5C%22unclassified%5C%22%2C%5C%22targeting%5C%22%5D%22%2C%22key%22%3A%223e8df365-6f2c-4c1a-80ad-d5c902d78b97%22%7D; PHPSESSID=1lri84b80glrea6c7jfo4pc53o; _gid=GA1.2.857438531.1689414271; cf_clearance=t7MwWSqLcCEwpRYz3NmAqYPgjYP0PyhfIM_aHLDmGjI-1689459386-0-250; __cf_bm=GHTcYoAfD3ehTxqONFaM1TSP8M1UxhmHYlBxFhmbCec-1689459397-0-AbFgEfnp8f6wyhkmHznOWD8JfY+VPRgVd9m48Fq4KTqbBXyCbDC7WoBqmydmxAfZWg==; _gat_UA-724652-3=1; _ga_D931ERQW91=GS1.1.1689459385.21.1.1689459397.0.0.0; _ga=GA1.1.1096950485.1688928483';
+        $cookie_consent = 'CookieScriptConsent=%7B%22googleconsentmap%22%3A%7B%22ad_storage%22%3A%22targeting%22%2C%22analytics_storage%22%3A%22performance%22%2C%22functionality_storage%22%3A%22functionality%22%2C%22personalization_storage%22%3A%22functionality%22%2C%22security_storage%22%3A%22functionality%22%7D%2C%22action%22%3A%22accept%22%2C%22categories%22%3A%22%5B%5C%22unclassified%5C%22%2C%5C%22targeting%5C%22%5D%22%2C%22key%22%3A%223e8df365-6f2c-4c1a-80ad-d5c902d78b97%22%7D; PHPSESSID=1lri84b80glrea6c7jfo4pc53o; _gid=GA1.2.857438531.1689414271; cf_clearance=GecacmAmkFKSrBBBuu13366sqgQU2MUG9DmW5Zhx1iY-1689464079-0-250; __cf_bm=J0UzSUXpem0NE_K7em3r5ADCIcaTlD6tCbIIi2Ebqms-1689464088-0-AbtgzujTGd18vl7ojYY1+i4uN5ZA8dsJJFjbvHzfqOlrEhET1yW+6zre6NJoEbzVKA==; _gat_UA-724652-3=1; _ga_D931ERQW91=GS1.1.1689464079.22.1.1689464088.0.0.0; _ga=GA1.1.1096950485.1688928483';
         $user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
 
-        // Set the request headers
+        // Set request headers
         $header_elements = [
             'content-type: multipart/form-data; boundary=----WebKitFormBoundary7DhR5jphY5R9QUgD',
             'cookie: ' . $cookie_consent,
             'user-agent: ' . $user_agent
         ];
-
-//        $registration_code = 304565690;
-        // Set the request data
-        $data = "------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"word\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"code\"\r\n\r\n304565690\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"codepvm\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"city\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"search_terms\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"street\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"employeesMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"employeesMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salaryMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salaryMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"debtMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"debtMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"transportMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"transportMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salesRevenueMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salesRevenueMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"netProfitMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"netProfitMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"registeredFrom\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"registeredTo\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"search_terms\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"order\"\r\n\r\n1\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"resetFilter\"\r\n\r\n0\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD--\r\n";
-//        $data = "name=&word=&code=" . $registration_code . "&codepvm=&city=&search_terms=&street=&employeesMin=&employeesMax=&salaryMin=&salaryMax=&debtMin=&debtMax=&transportMin=&transportMax=&salesRevenueMin=&salesRevenueMax=&netProfitMin=&netProfitMax=&registeredFrom=&registeredTo=&search_terms=&order=1&resetFilter=0";
-        // Build the stream context options
+        
+        // Set request data
+        $registration_code = 152540575;
+        $data = "------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"word\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"code\"\r\n\r\nPUT_REGISTRATION_CODE_HERE\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"codepvm\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"city\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"search_terms\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"street\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"employeesMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"employeesMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salaryMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salaryMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"debtMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"debtMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"transportMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"transportMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salesRevenueMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"salesRevenueMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"netProfitMin\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"netProfitMax\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"registeredFrom\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"registeredTo\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"search_terms\"\r\n\r\n\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"order\"\r\n\r\n1\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD\r\nContent-Disposition: form-data; name=\"resetFilter\"\r\n\r\n0\r\n------WebKitFormBoundary7DhR5jphY5R9QUgD--\r\n";
+        $form_data = str_replace("PUT_REGISTRATION_CODE_HERE", $registration_code, $data);
+        
+        // Options from stream_context
         $options = [
             'http' => [
                 'header' => implode(PHP_EOL, $header_elements),
                 'method' => 'POST',
-                'content' => $data,
+                'content' => $form_data,
             ],
         ];
 
@@ -42,7 +44,10 @@ class HomeController extends AbstractController {
         $html_array = explode(PHP_EOL, $html_chunk);
 
         foreach ($html_array as $key => $val) {
-            if (strpos($val, "company-title d-block")) {
+            if (strpos($val, "No records found. Please refine the search criteria") !== false){
+                print_r("No records found. Please provide valid Registration Code"); die();
+            }
+            if (strpos($val, "company-title d-block") !== false) {
                 $index = $key;
                 break;
             }
@@ -70,14 +75,10 @@ class HomeController extends AbstractController {
         $company_profile_html_chunk = file_get_contents($href, false, $company_profile_context);
         $company_details = $this->_retrieve_company_details($company_profile_html_chunk);
 
-        print_r($company_details['mobile']);
-        die();
-
-        // Companyname, RC, Vat, Address, Mobile Phone.
-//        return $this->render('home/index.html.twig', [
-//                    'controller_name' => 'HomeController',
-//                    'content' => $response
-//        ]);
+        return $this->render('home/index.html.twig', [
+                    'controller_name' => 'HomeController',
+                    'content' => $company_details
+        ]);
     }
 
     private function _retrieve_company_details($details): array {
@@ -87,7 +88,7 @@ class HomeController extends AbstractController {
 
         foreach (Constants::IDENTIFY_COMPANY_DETAILS as $info => $props) {
             foreach ($company_profile_html_array as $key => $val) {
-                if (strpos($val, $props['IDENTIFYING_KEY'])) {
+                if (strpos($val, $props['IDENTIFYING_KEY']) !== false) {
                     $info_index = $key + $props['NODE_TO_TRAVEL'];
                     break;
                 }
