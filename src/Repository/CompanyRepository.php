3c5777f76d35aa7e97d7a0c67a3132514f36c2af
arefeen <arefeen.git@gmail.com>
2023-07-20 20:51:53 +0600

    1. Registration code : unique properties and validation added. 2. Vat and Mobile exception/issue fixed.

--- src/Repository/CompanyRepository.php
+++ src/Repository/CompanyRepository.php
@@ -15,8 +15,6 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Company[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 
-// Arefeen : Use  for Business Logic.
-
 class CompanyRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -24,28 +22,14 @@ class CompanyRepository extends ServiceEntityRepository
         parent::__construct($registry, Company::class);
     }
 
-//    /**
-//     * @return Company[] Returns an array of Company objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function isRegistrationCodeExists(int $registrationCode): bool
+    {
+        $companies = $this->findBy(['registration_code' => $registrationCode]);
+        
+        if (!empty($companies)){
+            return true;
+        }
 
-//    public function findOneBySomeField($value): ?Company
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        return false;
+    }
 }
