f327d417ff58d9131da7f84f4a113d03952c2798
arefeen <arefeen.git@gmail.com>
2023-08-06 22:38:10 +0600

    migration script updated.

--- migrations/Version20230805235227.php
+++ migrations/Version20230805235227.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230719134615 extends AbstractMigration
+final class Version20230805235227 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230719134615 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE company ADD deleted TINYINT(1) NOT NULL, DROP deleted_at');
+        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, registration_code INT NOT NULL, name VARCHAR(255) NOT NULL, details JSON NOT NULL, finances LONGTEXT DEFAULT NULL, deleted TINYINT(1) NOT NULL, updated_at DATETIME DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_4FBF094FB82B2744 (registration_code), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE company ADD deleted_at DATETIME DEFAULT CURRENT_TIMESTAMP, DROP deleted');
+        $this->addSql('DROP TABLE company');
     }
 }
